4b6bfca3a02daa281a30f8724d29f502fd7387f7
Lucas Veiga Mattos <lucasveigamattos@gmail.com>
2023-04-21 18:15:50 -0300

    Server configuration, and bug fixes like: tsnd errors.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import bodyParser from "body-parser" // Se if we will need in the future.
+import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
@@ -8,3 +8,19 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import {fileURLToPath} from "url"
+
+// Configurations
+const __fileName = fileURLToPath(import.meta.url)
+const __dirName = path.dirname(__fileName)
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit: "30mb"}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+app.use("/assets", express.static(path.join(__dirName, "public/assets")))
