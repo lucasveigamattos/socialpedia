e841ad3b33034eb6b239436c3fb741a025ff5e2d
Lucas Veiga Mattos <lucasveigamattos@gmail.com>
2023-05-07 18:38:05 -0300

    Register page, login page, register form, login form, code updated

--- web/src/pages/Register/index.tsx
+++ web/src/pages/Register/index.tsx
@@ -9,16 +9,15 @@ function Register() {
     const isNonMobileScreen = useMediaQuery("(min-width: 1000px)")
 
     return (
-        <div>
+        <div className="h-full" style={{backgroundColor: theme.pallete.background.default}}>
             <nav className="text-center py-[1.6rem] px-[6%]" style={{backgroundColor: theme.pallete.background.alternative}}>
-                <Typography fontWeight="bold" fontSize="3.2rem" color="primary" className="cursor-pointer hover:opacity-75 duration-[300ms]">Socialpedia</Typography>
+                <Typography fontWeight="bold" fontSize="3.2rem" color="primary">Socialpedia</Typography>
             </nav>
 
             <div className="p-[3.2rem] rounded-[2.4rem] my-[3.2rem] mx-auto" style={{width: isNonMobileScreen ? "50%" : "93%", backgroundColor: theme.pallete.background.alternative}}>
                 <Typography fontWeight="500" variant="h5" sx={{mb: "2.4rem"}}>Welcome to Socialpedia, the social media for you!</Typography>
+                <RegisterForm/>
             </div>
-
-            <RegisterForm/>
         </div>
     )
 }
